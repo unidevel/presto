b0483ca49597d12cbe6d9b23fb09e80ad30fd2fb
unidevel <unidevel@hotmail.com>
2026-03-20 17:29:20 +0000

    feat(01-01): add DeltaAnalyzeProperties for ANALYZE statement support
    - Create DeltaAnalyzeProperties class following HiveAnalyzeProperties pattern
    - Add getAnalyzeProperties() to DeltaConnector
    - Register DeltaAnalyzeProperties in DeltaModule for DI
    
    This establishes the foundation for ANALYZE statement support
    enabling users to collect table and column statistics for
    query optimization.

--- presto-delta/src/main/java/com/facebook/presto/delta/DeltaConnector.java
+++ presto-delta/src/main/java/com/facebook/presto/delta/DeltaConnector.java
@@ -3,7 +3,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -36,8 +36,7 @@ import java.util.List;
 import static com.facebook.presto.delta.DeltaTransactionHandle.INSTANCE;
 import static java.util.Objects.requireNonNull;
 
-public class DeltaConnector
-        implements Connector
+public class DeltaConnector implements Connector
 {
     private static final Logger log = Logger.get(DeltaConnector.class);
 
@@ -49,6 +48,7 @@ public class DeltaConnector
     private final DeltaPageSourceProvider pageSourceProvider;
     private final DeltaPlanOptimizerProvider planOptimizerProvider;
     private final DeltaTableProperties deltaTableProperties;
+    private final DeltaAnalyzeProperties deltaAnalyzeProperties;
 
     @Inject
     public DeltaConnector(
@@ -59,7 +59,8 @@ public class DeltaConnector
             HiveCommonSessionProperties hiveCommonSessionProperties,
             DeltaPageSourceProvider pageSourceProvider,
             DeltaPlanOptimizerProvider planOptimizerProvider,
-            DeltaTableProperties deltaTableProperties)
+            DeltaTableProperties deltaTableProperties,
+            DeltaAnalyzeProperties deltaAnalyzeProperties)
     {
         this.lifeCycleManager = requireNonNull(lifeCycleManager, "lifeCycleManager is null");
         this.metadata = requireNonNull(metadata, "metadata is null");
@@ -69,6 +70,7 @@ public class DeltaConnector
         this.pageSourceProvider = requireNonNull(pageSourceProvider, "pageSourceProvider is null");
         this.planOptimizerProvider = requireNonNull(planOptimizerProvider, "planOptimizerProvider is null");
         this.deltaTableProperties = requireNonNull(deltaTableProperties, "tableProperties is null");
+        this.deltaAnalyzeProperties = requireNonNull(deltaAnalyzeProperties, "analyzeProperties is null");
     }
 
     @Override
@@ -113,6 +115,12 @@ public class DeltaConnector
     public List<PropertyMetadata<?>> getTableProperties()
     {
         return deltaTableProperties.getTableProperties();
+    }
+
+    @Override
+    public List<PropertyMetadata<?>> getAnalyzeProperties()
+    {
+        return deltaAnalyzeProperties.getProperties();
     }
 
     @Override
